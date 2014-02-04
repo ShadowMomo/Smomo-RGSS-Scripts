3657f598d913239ad66ece00c09fb6429fd67e09
ShadowMomo <ShadowMomo@Live.com>
2014-02-04 20:01:02 +0800

    Update Keep Map Status.rb

--- Keep Map Status.rb
+++ Keep Map Status.rb
@@ -10,25 +10,19 @@
 #==============================================================================
 # ● 使用方法
 #   将此脚本插入到其他脚本以下，Main以上
-#   可以设定不保存状态的特殊地图
+#   下方可以设定不保存状态的特殊地图
 #==============================================================================
 # ● 更新
+#   V 1.1 2014.02.04 增强兼容性
 #   V 1.0 2014.01.22 新建
 #==============================================================================
 # ● 声明
 #   本脚本来自【影月千秋】，使用、修改和转载请保留此信息
 #==============================================================================
- 
-#==============================================================================
-# ■ Smomo::SaveMapStatus
-#==============================================================================
 module Smomo
   module SaveMapStatus
-    NoSaveEvents = [3,7,5]
-    # 在哪些地图不保存事件状态
-     
-    NoSaveVehicles = [7,11]
-    # 在哪些地图不保存载具状态
+    NoSave = [1]
+    # 在哪些地图不保存状态
 #=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+#
 #------------------------------------------------------------------------------#
 #                               请勿跨过这块区域                                #
@@ -36,28 +30,29 @@ module Smomo
 #+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=#
   end
 end
-#==============================================================================
-# ■ Game_Map
-#==============================================================================
 class Game_Map
-  attr_accessor :mo_events_status
-  attr_accessor :mo_vehicles_status
-  #--------------------------------------------------------------------------
-  # ● 保存状态
-  #--------------------------------------------------------------------------
+  attr_accessor :mo_save_map_status
   alias :mo_save_status_setup :setup
   def setup(map_id)
-    @mo_events_status ||= {}
-    @mo_vehicles_status ||= {}
-    @mo_events_status[@map_id] = @events.clone unless
-    Smomo::SaveMapStatus::NoSaveEvents.include?(@map_id)
-    @mo_vehicles_status[@map_id] = @vehicles.clone unless
-    Smomo::SaveMapStatus::NoSaveVehicles.include?(@map_id)
+    @mo_save_map_status ||= {}
+    @mo_save_map_status[@map_id] ||= {}
+    no_save_list = [:@screen, :@interpreter, :@display_x, :@display_y,
+    :@parallax_name, :@battleback1_name, :@battleback2_name, :@name_display,
+    :@need_refresh]
+    instance_variables.each do |s|
+      next if no_save_list.include?(s) ||
+      Smomo::SaveMapStatus::NoSave.include?(@map_id)
+      @mo_save_map_status[@map_id][s] = instance_variable_get(s).clone rescue
+      instance_variable_get(s)
+    end
     mo_save_status_setup(map_id)
-    @events = @mo_events_status[@map_id].clone unless
-    @mo_events_status[@map_id].nil? 
-    @vehicles = @mo_vehicles_status[@map_id].clone unless
-    @mo_vehicles_status[@map_id].nil?
+    @mo_save_map_status[@map_id] ||= {}
+    instance_variables.each do |s|
+      next if no_save_list.include?(s) ||
+      Smomo::SaveMapStatus::NoSave.include?(@map_id)
+      instance_variable_set(s, @mo_save_map_status[@map_id][s]) unless
+      @mo_save_map_status[@map_id][s].nil?
+    end
   end
 end
 #==============================================================================#
