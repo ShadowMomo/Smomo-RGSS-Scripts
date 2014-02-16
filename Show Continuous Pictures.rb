cfaf8ac99196cfaf4a7f014aade063b097706c37
ShadowMomo <ShadowMomo@Live.com>
2014-02-16 16:19:21 +0800

    Update Show Continuous Pictures.rb

--- Show Continuous Pictures.rb
+++ Show Continuous Pictures.rb
@@ -1,7 +1,7 @@
 #==============================================================================
 # ■ 播放连续图片
 #  作者：影月千秋
-#  版本：V 1.0
+#  版本：V 1.1
 #  适用：VA
 #------------------------------------------------------------------------------
 # ● 简介
@@ -21,8 +21,13 @@
 #   在这个例子中 也就是：
 #    picmovie("ani", 100, 150, 20, 3)
 #   代表总共有20张图片 显示在(100,150)这个地方 图片在ani文件夹内 每张停留3帧
+#   也可以使用
+#    picmovie2(文件夹名, 事件ID, 图片张数, 每张图片停留的帧数, 显示端口)
+#   将在指定ID的事件上播放动画 事件ID为0 则为当前事件 为-1 则为玩家
+#   注意这个是picmovie2 而不是picmovie
 #==============================================================================
 # ● 更新
+#   V 1.1 2014.02.16 新增了在指定事件播放的功能
 #   V 1.0 2014.02.15 新建
 #==============================================================================
 # ● 声明
@@ -47,6 +52,23 @@ class Game_Interpreter
     sp = Sprite.new(viewport)
     sp.x, sp.y = x, y
     seq = 1
+    until seq >= fcount
+      sp.bitmap.dispose if sp.bitmap
+      sp.bitmap = Cache.moviepics(folder, seq)
+      wcount.times{Graphics.update; Fiber.yield}
+      seq += 1
+    end
+  rescue
+    msgbox "错误信息：#{$!}\n播放到第#{seq}张,在动画#{folder}" if $TEST || $BTEST
+  ensure
+    sp.dispose rescue nil
+  end
+  def picmovie2(folder, cid, fcount, wcount = 1, viewport = nil)
+    sp = Sprite.new(viewport)
+    seq = 1
+    sp.bitmap = Cache.moviepics(folder, seq)
+    sp.ox, sp.oy = sp.bitmap.width / 2, sp.bitmap.height / 2
+    sp.x, sp.y = get_character(cid).screen_x, get_character(cid).screen_y
     until seq >= fcount
       sp.bitmap.dispose if sp.bitmap
       sp.bitmap = Cache.moviepics(folder, seq)
