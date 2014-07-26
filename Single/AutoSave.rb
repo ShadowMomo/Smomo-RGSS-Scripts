407d17619a143d17519c5d38f19e6fd19b4690cc
ShadowMomo <ShadowMomo@Live.com>
2014-07-26 14:05:09 +0800

    aotosave 1.8

--- Single/AutoSave.rb
+++ Single/AutoSave.rb
@@ -1,20 +1,19 @@
-#==============================================================================
-# ■ 自动存档
+﻿#==============================================================================
+# ** 自动存档
 #  作者：影月千秋
-#  版本：V 1.7
-#  最近更新：2014.04.07
 #  适用：VA
-#  要求：Smomo脚本核心
+#  要求：Smomo脚本核心 1.0+
 #------------------------------------------------------------------------------
-# ● 简介
-#  在很多解谜、生存、养成类游戏中，不需要玩家主动存档，而是由系统自动保存进度，这时
-# 就可以使用此脚本
-#  你可以设定脚本，使每次场所移动时自动存档（记录场景），也可以通过下面的事件脚本，
-# 配合其他时间系统使用（记录时间），根据你的设定，还可以做其他用途
+# * 简介
+#  在很多解谜、生存、养成类游戏中，不需要玩家主动存档，而是由系统自动保存进度
+# 这时就可以使用本脚本
+#  你可以设定脚本，使每次场所移动时自动存档（记录场景）
+#  也可以通过下面的事件脚本，配合其他时间系统使用（记录时间）
+#  根据你的设定，还可以做其他用途
 #  本脚本需要Smomo脚本核心，请到此下载：http://tinyurl.com/l9kvg2p
 #  如果链接失效，请到bbs.66rpg.com，@余烬之中 或 @影月千秋
 #==============================================================================
-# ● 使用方法
+# * 使用方法
 #  将此脚本插入到其他脚本以下，Main以上
 #  在下面进行相关设定，就可以达成基本使用效果
 #  另外：
@@ -35,7 +34,8 @@
 #     例：
 #      Smomo.autosave(:ReadAllowed, :get) 获取ReadAllowed的信息
 #==============================================================================
-# ● 更新
+# * 更新
+#   V 1.8 2014.07.26 添加注释 优化名称正则式
 #   V 1.7 2014.04.07 规范化脚本 纳入Smomo登记 需要依赖Smomo脚本核心
 #   V 1.6 2013.12.22 修正算法 规范化脚本
 #   V 1.5 2013.10.05 小修改，降低和其他脚本冲突的可能 新增功能
@@ -44,7 +44,7 @@
 #   V 1.2 2013.09.20 修正$ADSLO无效的BUG 添加详细备注
 #   V 1.0 2013.09.19 新建 公开
 #==============================================================================
-# ● 声明
+# * 声明
 #   本脚本来自【影月千秋】，使用、修改和转载请保留此信息
 #==============================================================================
 
@@ -54,14 +54,17 @@ elsif $smomo["AutoSave"].nil?
 $smomo["AutoSave"] = true
 
 #===============================================================================
-# ■ Smomo
+# ** Smomo
 #===============================================================================
 module Smomo
   #=============================================================================
-  # ■ Smomo::AutoSave
+  # ** Smomo::AutoSave
   #=============================================================================
   class AutoSave
     attr_accessor :sets
+    #------------------------------------------------------------------------
+    # * 初始化
+    #------------------------------------------------------------------------
     def initialize
       @sets = {
         LoadWhenStart: true,
@@ -96,71 +99,96 @@ module Smomo
         GameCompleteKillFile: true,
         # 事件【返回标题画面】时是否删除自动存档
       
+        Saving: false
+        # 标志，勿动
+      }
 #=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+#
 #------------------------------------------------------------------------------#
-#                               请勿跨过这块区域                                #
+                               "请勿跨过这块区域"
 #------------------------------------------------------------------------------#
 #+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=#
-        Saving: false
-      }
+    end
+    #------------------------------------------------------------------------
+    # * 用户设置
+    #------------------------------------------------------------------------
+    def self.function(type, value = nil)
+      case type
+      when :save
+        DataManager.mo_auto_save_game
+      when :load
+        if DataManager.mo_auto_load_game
+          SceneManager.scene.fadeout_all
+          $game_system.on_after_load
+          SceneManager.goto(Scene_Map)
+        end
+      when :kill
+        DataManager.mo_autosave_file_kill
+      else
+        value == :get ? $mo_autosave[type] : ($mo_autosave[type] = value)
+      end
     end
   end
+  module_function
   #--------------------------------------------------------------------------
-  # ● 进行自动存档
+  # * 入口
   #--------------------------------------------------------------------------
-  def self.autosave(type, value = nil)
-    case type
-    when :save
-      DataManager.mo_auto_save_game
-    when :load
-      if DataManager.mo_auto_load_game
-        SceneManager.scene.fadeout_all
-        $game_system.on_after_load
-        SceneManager.goto(Scene_Map)
-      end
-    when :kill
-      DataManager.mo_autosave_file_kill
-    else
-      value == :get ? $mo_autosave[type] : ($mo_autosave[type] = value)
-    end
+  def autosave(*a, &b)
+    Smomo::AutoSave.function(*a, &b)
   end
 end
 #==============================================================================
-# ■ DataManager
+# ** DataManager
 #==============================================================================
 class << DataManager
+  #--------------------------------------------------------------------------
+  # * 获取存档信息
+  #--------------------------------------------------------------------------
   _def_ :create_game_objects do |*args|
     $mo_autosave = load_data("Data/MoAutosaveSets.rvdata2") rescue
     Smomo::AutoSave.new.sets
   end
-
+  #--------------------------------------------------------------------------
+  # * 保存设置
+  #--------------------------------------------------------------------------
   def mo_autosave_save_sets
     save_data($mo_autosave, "Data/MoAutosaveSets.rvdata2")
   rescue
     File.delete("Data/MoAutosaveSets.rvdata2") rescue nil
   end
-
+  #--------------------------------------------------------------------------
+  # * 生成存档名
+  #--------------------------------------------------------------------------
   _def_ :make_filename, :v do |old, *args|
-    $mo_autosave[:Saving] && !$mo_autosave[:ReadAllowed] ? 
-    old.gsub(/save\d+/i){"MoAutoSave"} : old
+    $mo_autosave[:Saving] && !$mo_autosave[:ReadAllowed] ?
+    "MoAutoSave" + old.match(/\.[^\.]*$/)[0] : old
   end
-
-  _def_ :save_game, :b do |*args| mo_autosave_save_sets end
-
+  #--------------------------------------------------------------------------
+  # * 存游戏前先存设置
+  #--------------------------------------------------------------------------
+  _def_ :save_game, :b do |*args|
+    mo_autosave_save_sets
+  end
+  #--------------------------------------------------------------------------
+  # * 自动保存
+  #--------------------------------------------------------------------------
   def mo_auto_save_game
     $mo_autosave[:Saving] = true
-    save_game($mo_autosave[:SaveID])
+    save_game $mo_autosave[:SaveID]
     $mo_autosave[:Saving] = false
   end
-
+  #--------------------------------------------------------------------------
+  # * 自动加载
+  #--------------------------------------------------------------------------
   def mo_auto_load_game
     $mo_autosave[:Saving] = true
-    s = Dir.glob(make_filename($mo_autosave[:SaveID])).empty? ? false : 
+    _ = Dir.glob(make_filename($mo_autosave[:SaveID])).empty? ? false : 
     [load_game($mo_autosave[:SaveID])]
     $mo_autosave[:Saving] = false
-    s
+    _
   end
-
+  #--------------------------------------------------------------------------
+  # * 删除自动存档
+  #--------------------------------------------------------------------------
   def mo_autosave_file_kill
     $mo_autosave[:Saving] = true
     File.delete(make_filename($mo_autosave[:SaveID])) rescue nil
@@ -168,18 +196,18 @@ class << DataManager
   end
 end
 #==============================================================================
-# ■ Game_Interpreter
+# ** Game_Interpreter
 #==============================================================================
 class Game_Interpreter
   _def_ :command_353, :b do |*args| Smomo.autosave(:kill) end
   _def_ :command_354, :b do |*args| Smomo.autosave(:kill) end
 end
 #==============================================================================
-# ■ Window_TitleCommand
+# ** Window_TitleCommand
 #==============================================================================
 class Window_TitleCommand
   _def_ :initialize do |*args|
-    (select_symbol(:mo_auto_save_continue) if
+    (select_symbol :mo_auto_save_continue if
     mo_auto_save_continue_enabled) rescue nil
   end
 
@@ -197,22 +225,22 @@ class Window_TitleCommand
   end
 end
 #==============================================================================
-# ■ Scene_Title
+# ** Scene_Title
 #==============================================================================
 class Scene_Title
   _def_ :create_command_window do |*args|
-    @command_window.set_handler(:mo_auto_save_continue,
-    lambda{Smomo.autosave(:load)})
+    @command_window.set_handler :mo_auto_save_continue,
+    lambda{Smomo.autosave :load}
   end
   
   _def_ :command_new_game, :c do |old, *args, &block|
     return Smomo.autosave(:load) if $mo_autosave[:LoadWhenStart] &&
     @command_window.mo_auto_save_continue_enabled
-    old.call(*args, &block)
+    old.call *args, &block
   end
 end
 #==============================================================================
-# ■ Scene_Map
+# ** Scene_Map
 #==============================================================================
 class Scene_Map
   _def_ :perform_transfer do |*args|
@@ -225,6 +253,6 @@ else # if $smomo
 end
 #==============================================================================#
 #=====                        =================================================#
-           "■ 脚 本 尾"
+            "脚 本 尾"
 #=====                        =================================================#
 #==============================================================================#
