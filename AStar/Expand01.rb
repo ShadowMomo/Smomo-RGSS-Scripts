5fa8fe9694b932ffbb8fb8dd42d70f688b3e4468
Shadow Momo <ShadowMomo@Live.com>
2014-08-15 15:48:57 +0800

    Update Expand01.rb

--- AStar/Expand01.rb
+++ AStar/Expand01.rb
@@ -12,14 +12,14 @@ class Game_Character
     route = RPG::MoveRoute.new
     route.repeat = false
     route.skippable = true
-    AStar.make_route([self.x, self.y], [tx, ty]).each do |d|
+    AStar.make_route([self.x, self.y], [tx, ty], self).each do |d|
       route.list.unshift RPG::MoveCommand.new.tap{|c| c.code = d >> 1}
     end
     force_move_route(route)
   end
   alias :astar_move_toward_character :move_toward_character
   def move_toward_character(character)
-    dir = AStar.make_route([x, y], [character.x, character.y]).shift
+    dir = AStar.make_route([x, y], [character.x, character.y], self).shift
     dir.nil? ? astar_move_toward_character(character) : move_straight(dir)
   end
 end
@@ -31,10 +31,9 @@ class Game_Event
   def move_type_toward_player
     if @astar_last_player_pos != [$game_player.x, $game_player.y]
       @astar_last_player_pos = $game_player.x, $game_player.y
-      @astar_route = AStar.make_route([x, y], @astar_last_player_pos)
+      @astar_route = AStar.make_route([x, y], @astar_last_player_pos, self)
     end
-    (dir = @astar_route.shift).nil? ?
-    astar_move_type_toward_player :
+    (dir = @astar_route.shift).nil? ? astar_move_type_toward_player :
     move_straight(dir)
   end
 end
