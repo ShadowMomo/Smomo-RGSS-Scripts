aedcff41cdfec011a46a1110a546be6dd2265802
Esphas <esphas@hotmail.com>
2017-02-05 19:37:35 +0800

    Update Calendar.rb

--- Single/Calendar.rb
+++ Single/Calendar.rb
@@ -2,17 +2,15 @@
 # ** 虚拟日历
 #  作者：影月千秋
 #  适用：VA
-#  要求：Smomo脚本核心 1.0+
 #------------------------------------------------------------------------------
 # * 简介：
 #  模拟时间流逝，并在地图（菜单）进行显示
 #  可以自定义计时规则和显示效果
 #  请为本脚本准备变量和开关，在下方进行设定
-#  本脚本需要Smomo脚本核心，请到此下载：http://tinyurl.com/l9kvg2p
-#  如果链接失效，请到rm.66rpg.com，@余烬之中 或 @影月千秋
+#  不出意外的话本脚本不再维护
 #------------------------------------------------------------------------------
 # * 使用方法：
-#  将此脚本插入到其他脚本以下，Main以上，在下面给出的设定区进行设定后即可
+#  将此脚本插入到其他脚本以下，Main 以上，在下面给出的设定区进行设定后即可
 #  在事件中操作指定的变量和开关，便可以获取和改变时间
 #  也可以通过事件脚本进行操作
 #  允许的脚本呼叫：
@@ -26,15 +24,11 @@
 #       【:prd】当前周期的序号，对应在周期名字（如:3）
 #       【:pname】当前周期的名字，与prd是对应的（如:周三）
 #   例：【Smomo.calendar(:zone)】获取时段名
-#  对于插件脚本：
-#   可以使用【Smomo::Calendar.routine.push(某方法)】来向列表中添加新方法
-#   例：【Smomo::Calendar.routine.push(lambda{ puts "一天过去了" })】
-#   列表中的方法会在一个时段周期结束时被调用
-#   有关时段周期，请参阅112行及以下
-#  更多内容，示例工程，请来发布帖查看：
+#  发布帖：
 #   http://rm.66rpg.com/thread-330684-1-1.html
 #------------------------------------------------------------------------------
 # * 版本
+#   V 4.0 2017.02.05 删掉了估计没什么人用的 routine；修复了周期更新异常的 bug
 #   V 3.4 2016.08.01 增加了一个人性化的报错提示: 未设置 Speed 对应变量
 #   V 3.3 2014.09.30 解决了时段计时与时间在变量干涉下不同步的问题
 #   V 3.2 2014.09.07 添加了公共接口routine
@@ -56,70 +50,59 @@
 #   本脚本由来自【影月千秋】，使用和转载请保留此信息
 #==============================================================================
 
-if $smomo["Core"].nil? || $smomo["Core"] < 1.0
-  msgbox "请先安装Smomo脚本核心！"; %x!start http://tinyurl.com/l9kvg2p!
-elsif $smomo["Calendar"].nil?
-$smomo["Calendar"] = true
+$smomo ||= {}
+if $smomo["Calendar"].nil?
+$smomo["Calendar"] = 4.0
 
 #==============================================================================
 # ** Smomo::Calendar
 #==============================================================================
-module Smomo::Calendar
-  Menu = true
-  # 是否在菜单内显示日历
-   # true 使用  false 不使用
-   # 如果预设不能满足要求 请自行在下方自定义区更改
+module Smomo
+module Calendar
   
-  Map = :bottom
-  # 是否在地图上显示日历
-   # :top 顶部  :bottom 底部  :none 不使用
-   # 如果预设不能满足要求 请自行在下方自定义区更改
+  # 一般情况下可以不予配置直接使用本系统
+  # 但为了获得比较理想的效果，建议仔细进行配置
+  # 配置比较繁琐，请务必耐心阅读注释
   
   Use = 16
-  # 设定占用开关，只有当此开关打开时，才会计时
-   # ！打开开关前 请确保下方Speed对应的变量不为0！
-  
-  Var = 81
-  # 设定占用变量的起始编号 会占用以这个号码为开端的连续几个号码对应的游戏变量
-  
-  # 继续设置需要了解的事实：
-  #  脚本内部用一个数字累加计时 然后依次推进到下一单位 把最开始的数字叫计时变量
+  # [开关 ID] 只有当此开关打开时，才会计时
+   # 打开开关前，请确保下方 Speed 对应的变量不为 0
   
   Speed = 100
-  # 设定游戏时间进行速度占用变量，代表经过多少帧后游戏内部计时变量增加一
-   # 一般情况下 1秒60帧
-   # 再打开Use对应的开关前 请确保这个常量不为0
-   # 请确保这个常量不与上面Var的常量（及其包括的范围内的变量）重合
-
+  # [变量 ID] 此变量的值代表经过多少帧后最小计时单位增加一
+   # 一般情况下，1 秒 = 60 帧
+  
   System = [
-    # ["单位", 满多少进一（比最大值大一）, 是否以零起始（true/false）],
+    # ["单位名", 满多少进一, 是否以零起始],
     ["分", 60, true],
     ["时", 24, true],
     ["日", 30],
     ["月", 12],
     ["年", 9999],
   ]
-  # 设定计时制，最后一个数据的单位与计时变量的单位统一
-   # 可以突破公元历法的限制
-    # 比如：636号时间线3145纪6887年6月4日 21:39:44:03
-   # ！这里有几项 上面的Var就会占用几个变量！
-   # ！从上往下！ 单位由小到大 如：分-->时-->日-->月-->年
-   # 有的单位允许以0为值 比如 3:00 有的不行 比如 3月1日
-    # 默认不允许0值 如果需要允许 请将“以零起始”填为【true】
+  # 设定计时制，排在前面的单位小于后面的单位
+   # 有的单位允许零值，比如 3:00，有的不行，比如 3 月 1 日
+    # 默认不允许零值，如果允许，请将“是否以零起始”填为[true]
+   # 称第一个单位为最小计时单位
   
-  Start = [23, 9, 1, 8, 2016]
-  # 设置游戏起始时间 与上面的单位从上到下依次对应
+  Var = 81
+  # [变量 ID] 设定记时制占用变量的起始编号
+   # 会占用以这个号码为首的连续数个变量
+    # 占用的变量个数即之前 System 中设置的单位的个数
+  
+  Start = [38, 12, 5, 2, 2017]
+  # 设置游戏起始时间，与上面 System 中的单位从上到下依次对应
   
   PeriodName = ["周日", "周一", "周二", "周三", "周四", "周五", "周六"]
-  # 日期的别名 每经过一个周期都会自动推进并显示下一个名字
-   # 别名列表长度不固定 可以自行添加或缩短
+  # 周期
+   # 每经过一定长度的最小计时单位，会循环推进到下一个周期
+   # 具体经过的最小计时单位数在 Zone 中设定
    
-  Start_PeriodName = 6
-  # 确定游戏起始时的那个周期的别名是什么 0为第一个别名
-   # 一定要用数字来指代 不要用"字符串"
+  Start_PeriodName = 0
+  # 游戏起始时的周期，用序号指代（从 0 开始数）
   
   TimeZone = true
-  # 是否启用时段功能， 即一个周期内是否有各时段的区分（如：凌晨-中午-黄昏-黑夜）
+  # [true/false] 是否启用时段功能，即一个周期内是否有各时段的区分
   
   Zone = [
   # [时段长度, Tone.new(红, 绿, 蓝, 灰), "名称"],
@@ -133,43 +116,52 @@ module Smomo::Calendar
     [300, Tone.new( -75,-100,   0,  75), "夜晚"], # 1140...1440 则1440为周期长度
   ]
   # 设置时段周期内各时段的长度、色调及名称
-   # 时段长度以计时变量的单位为单位
-  # 到达对应时段时 会自动改变画面的色调
-   # 【所有时段的长度加起来就是周期长度！】
-   # 【所以，就算不需要使用时段功能，也一定要保证时段长度之和是一个合适的值！】
-  # 当一次时段轮回完毕 就会推移到下一个周期（如：周三-->周四）
+   # 时段长度以最小计时单位为单位
+   # 到达对应时段时 会自动改变画面的色调
+   # 所有时段的长度加起来就是周期长度
+    # 所以，就算不需要使用时段功能，也一定要保证时段长度之和是一个合适的值
   
   IndoorMap = /<IndoorMap>/i
   # 用于匹配室内地图备注的正则式
-   # 默认为/<IndoorMap>/i
-   # 在地图的备注上写【<IndoorMap>】就可以标记其为室内地图
-   # 不区分大小写
-  # 处于室内地图时，不会受到时段色调的影响
+   # 默认为 /<IndoorMap>/i
+    # 即在地图的备注上写 <IndoorMap> 就可以标记其为室内地图，不区分大小写
+   # 处于室内地图时，不会受到时段色调的影响
+  
+  Menu = true
+  # [true/false] 是否在菜单内显示日历
+   # true 使用  false 不使用
+   # 如果预设不能满足要求 请自行在下方自定义区更改
+  
+  Map = :bottom
+  # [:top/:bottom/:none] 是否在地图上显示日历
+   # :top 顶部  :bottom 底部  :none 不使用
+   # 如果预设不能满足要求 请自行在下方自定义区更改
   
   Format = {
-    # 菜单用的格式
+    # 菜单日历用的格式
     menu: %W!
             <4年>.<2月>.<2日>
             \\I[234]___<Period>
             <2时>:<2分>_<Zone>
           !,
-    # 地图用的格式
+    # 地图日历用的格式
     map:  %W!
             \\I[234]公元<4年>年<2月>月<2日>日___<Period>___<Zone>_<2时>:<2分>
           !
   }
-  # 输出格式 此处写多少行 游戏内就会依次对应输出多少行
+  # 日历输出格式，此处写多少行，游戏内就会依次对应输出多少行
    # 每一行的内部不要使用空格（ ），空格用下划线（_）代替
    # 尽量不要使用英文感叹号(!)，如果一定要用请在之前加上反斜杠（\!）
-   # 使用【<符号>】的形式来代替指定的数据 其他的文字用于修饰
-   # 可用的符号
+   # 使用 <符号> 的形式来代替指定的数据，其他的文字用于修饰
+   # 可用的符号：
     # 在上面计时制中设定的单位 可以加上一个数字来代表格式化位数
-     # 如：<2时>:<2分> 显示 09:33 <时>:<分> 显示 9:33 
+     # 如：<2时>:<2分> 显示 09:33，<时>:<分> 显示 9:33 
     # <Period> 当前周期的名字（如：星期三）
     # <Zone>   当前时段的名字（如：早晨）
    # 两个符号必须用两对<>分别括起来 不能写在同一对<>内
-    # 允许使用转义字符 但是必须有两个反斜杠
-     # 如 显示图标 \I[3] 应写成 \\I[3]
+   # 允许使用转义字符 但是必须有两个反斜杠
+    # 如 显示图标 \I[3] 应写成 \\I[3]
+end
 end
 
 #==============================================================================
@@ -188,11 +180,9 @@ class Window_MoMenuCalendar < Window_Base
       self.opacity = 0
       return
     end
-    Smomo::Calendar.ensure_time_legal
-    Smomo::Calendar.check_period_and_zone
     height = Format[:menu].size * 30 + 20
     super(0, gold_window.y - height, gold_window.width, height)
-    format = Smomo.deep_clone(Format[:menu])
+    format = Marshal.load Marshal.dump(Format[:menu])
     format.each_with_index do |t, l|
       t.gsub!(/_/){" "}
       System.each_with_index do |(u, m), i|
@@ -243,12 +233,10 @@ class Window_MoMapCalendar < Window_Base
   end
   
   def refresh
-    Smomo::Calendar.ensure_time_legal
-    Smomo::Calendar.check_period_and_zone
     contents.clear
     contents.gradient_fill_rect(0, 0, @sprite.width, @sprite.height,
     Color.new(30, 30, 30), Color.new(0, 0, 0, 0))
-    format = Smomo.deep_clone(Format[:map])
+    format = Marshal.load Marshal.dump(Format[:map])
     format.each_with_index do |t, l|
       t.gsub!(/_/){" "}
       System.each_with_index do |(u, m), i|
@@ -275,7 +263,9 @@ end
 # ** Scene_Menu 建立菜单场景的窗口
 #==============================================================================
 class Scene_Menu
-  _def_ :start do |*args|
+  alias ewc_start start
+  def start
+    ewc_start
     return unless Smomo::Calendar::Menu
     # 在下面添加自定义内容
     @mocalendar_window = Window_MoMenuCalendar.new(@gold_window)
@@ -285,7 +275,9 @@ end
 # ** Scene_Map 建立地图场景的窗口
 #==============================================================================
 class Scene_Map
-  _def_ :create_all_windows do |*args|
+  alias ewc_create_all_windows create_all_windows
+  def create_all_windows
+    ewc_create_all_windows
     return if Smomo::Calendar::Map == :none
     # 在下面添加自定义内容
     @mocalendar_window = Window_MoMapCalendar.new 
@@ -301,94 +293,94 @@ end
 # ** Smomo::Calendar
 #==============================================================================
 module Smomo::Calendar
-  # 预处理
+  #
   PeriodSize = Zone.inject(0){|s, (l)| s + l}
   Zone.each_with_index{|z, i| z[0] += i == 0 ? 0 : Zone[i - 1][0]}
-  _ = Smomo.deep_clone(Zone)
+  _ = Marshal.load Marshal.dump Zone
   _.each_with_index{|z, i| Zone[i][0] = (i == 0 ? 0 : _[i - 1][0])...z[0]}
+  _ = 1
+  System.each_with_index do |u, i|
+    _ *= i == 0 ? 1 : System[i - 1][1]
+    u[2] = !!u[2]
+    u[3] = _
+  end
+  #
   class << self
-    attr_reader :zone, :prd, :ticking
-    attr_accessor :period, :routine
+    attr_reader :zone, :prd
+    attr_accessor :period
     define_method(:data){[@zone, @tone, @need_change, @period, @prd]}
     define_method(:data=){|d| @zone, @tone, @need_change, @period, @prd = d}
     define_method(:function){|type| type == :all ? data : eval(type.to_s)}
-    # 获知周期名
+    #
     def pname
       PeriodName[@prd]
     end
-    # 初始化
+    #
     def ini
-      @ticking = true
-      System.each_index{|i| $game_variables[Var + i] = Start[i]}
-      @ticking = false
-      @zone = ""
+      $game_variables.instance_exec do
+        System.each_index{|i| @data[Var + i] = Start[i]}
+      end
       @tone = Tone.new 0, 0, 0, 0
       @need_change = false
-      @period = init_period
       @prd = Start_PeriodName
-      check_period_and_zone
+      set_period
+      fire_in_the_hole
     end
-    # 初始化周期
-    def init_period
-      sta = Start.reverse
-      sta.each_with_index do |n, i|
-        next if i == sta.size - 1
-        sta[i + 1] += n * System.reverse[i + 1][1]
-      end
-      sta.reverse[0] % PeriodSize
+    #
+    def set_period
+      u = System.each_index.map{|i| $game_variables[Var + i]}
+      v = System.map{|a| a[3]}
+      @period =
+        u.zip(v).each_with_object(:*).map(&:inject).inject(:+) % PeriodSize
     end
-    # 重置周期
-    def reset_period
-      sta = System.each_index.map{|i| $game_variables[Var + i]}.reverse
-      sta.each_with_index do |n, i|
-        next if i == sta.size - 1
-        sta[i + 1] += n * System.reverse[i + 1][1]
-      end
-      @period = sta.reverse[0] % PeriodSize
-    end
-    # 计时
+    #
     def i_look_into_the_sky_as_time_passes_by
       return unless $game_switches[Use]
       return if $game_message.visible
-      if $game_variables[Speed].zero?
-        raise "请先将 Speed 对应变量设为非零值!"
-      end
+      raise "请先将 Speed 对应变量设为非零值!" if $game_variables[Speed].zero?
       return unless Graphics.frame_count % $game_variables[Speed] == 0
-      @ticking = true
       $game_variables[Var] += 1
-      @period += 1
+      fire_in_the_hole
+    end
+    #
+    def wtf var, ori, lat
+      @period += System[var - Var][3] * (lat - ori)
+      fire_in_the_hole
+    end
+    #
+    def fire_in_the_hole
       ensure_time_legal
       ensure_period_legal
       check_period_and_zone
       change_tone if TimeZone
-      @ticking = false
     end
-    # 确保变量在范围内
+    #
     def ensure_time_legal
-      @ticking = true
-      System.each_with_index do |(u, m, o), i|
-        while $game_variables[Var + i] > m - (o ? 1 : 0)
-          $game_variables[Var + i] -= m
-          $game_variables[Var + i + 1] += 1
-        end
-        while $game_variables[Var + i] < 1 - (o ? 1 : 0)
-          $game_variables[Var + i] += m
-          $game_variables[Var + i + 1] -= 1
+      $game_variables.instance_exec do
+        System.each_with_index do |(u, m, o), i|
+          while @data[Var + i] > m - (o ? 1 : 0)
+            @data[Var + i] -= m
+            @data[Var + i + 1] += 1
+          end
+          while @data[Var + i] < 1 - (o ? 1 : 0)
+            @data[Var + i] += m
+            @data[Var + i + 1] -= 1
+          end
         end
       end
-      @ticking = false
     end
-    # 确保时段在范围内
+    #
     def ensure_period_legal
-      [@routine.each(&:call), @prd += 1, @period = 0] if @period >= PeriodSize
-      @prd = 0 if @prd >= PeriodName.size
+      @prd += @period / PeriodSize
+      @period = @period % PeriodSize
+      @prd = @prd % PeriodName.size
     end
-    # 检查周期别名和时段
+    #
     def check_period_and_zone
       tone, @zone = Zone.find{|(r, t, n)| r.include?(@period)}[1, 2]
       @tone == tone ? nil : [@tone = tone, @need_change = true]
     end
-    # 改变画面色调
+    #
     def change_tone im = false, reset = false
       return unless @need_change || im
       @need_change = false
@@ -399,12 +391,11 @@ module Smomo::Calendar
         $game_map.screen.start_tone_change @tone, im ? 0 : 60
       end
     end
-    # 是否是室内地图
+    #
     def indoor?
       $game_map.instance_exec{ @map }.note =~ IndoorMap
     end
   end
-  Smomo::Calendar.routine = Array.new
 end
 #==============================================================================
 # ** Smomo.calendar(*a, &b)
@@ -416,7 +407,9 @@ end
 # ** Game_Switches
 #==============================================================================
 class Game_Switches
-  _def_ :on_change do
+  alias ewc_on_change on_change
+  def on_change
+    ewc_on_change
     Smomo::Calendar.change_tone true, true
   end
 end
@@ -424,45 +417,49 @@ end
 # ** Game_Variables
 #==============================================================================
 class Game_Variables
-  _def_ :[]=, :c do |old, variable_id, value|
-    if Smomo::Calendar.ticking
-      old.call variable_id, value
-    else
-      min = Smomo::Calendar::Var
-      max = min + Smomo::Calendar::System.size - 1
-      if variable_id.between? min, max
-        Smomo::Calendar.period += value - @data[variable_id]
-        old.call variable_id, value
-        Smomo::Calendar.ensure_time_legal
-        Smomo::Calendar.ensure_period_legal
-        Smomo::Calendar.check_period_and_zone
-        Smomo::Calendar.change_tone
-        Smomo::Calendar.reset_period
-      else
-        old.call variable_id, value
-      end
+  alias :ewc_set_var :[]=
+  def []= variable_id, value
+    min = Smomo::Calendar::Var
+    max = min + Smomo::Calendar::System.size - 1
+    if variable_id.between? min, max
+      Smomo::Calendar.wtf variable_id, @data[variable_id], value
     end
+    ewc_set_var variable_id, value
   end
 end
 #==============================================================================
 # ** Scene_Map
 #==============================================================================
 class Scene_Map
-  _def_ :update do |*args|
+  alias ewc_update update
+  def update
+    ewc_update
     Smomo::Calendar.i_look_into_the_sky_as_time_passes_by
   end
-  _def_ :post_transfer, :b do |*args| Smomo::Calendar.change_tone(true) end
+  alias ewc_post_transfer post_transfer
+  def post_transfer
+    Smomo::Calendar.change_tone(true)
+    ewc_post_transfer
+  end
 end
 #==============================================================================
 # ** DataManager
 #==============================================================================
 class << DataManager
-  _def_ :setup_new_game do Smomo::Calendar.ini end
-  _def_ :make_save_contents, :v do |contents, *args|
+  alias ewc_setup_new_game setup_new_game
+  def setup_new_game
+    ewc_setup_new_game
+    Smomo::Calendar.ini
+  end
+  alias ewc_make_save_contents make_save_contents
+  def make_save_contents
+    contents = ewc_make_save_contents
     contents[:mocalendar] = Smomo::Calendar.data
     contents
   end
-  _def_ :extract_save_contents do |contents, *args|
+  alias ewc_extract_save_contents extract_save_contents
+  def extract_save_contents contents
+    ewc_extract_save_contents contents
     Smomo::Calendar.data = contents[:mocalendar]
   end
 end
